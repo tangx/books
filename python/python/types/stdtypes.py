fbc4d8feb67ac0cacfe8bb896f36152c67245296
tangx <uyinn@live.com>
2025-03-06 13:18:33 +0000

    Update from https://github.com/tangx/gitbooks-python/commit/0479dbf3f1d9c5ba9b8e481c882bf3e923f02eb9

---
+++ python/python/types/stdtypes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File: stdtypes.py
+
+# Built-in types
+# https://docs.python.org/3/library/stdtypes.html
+
+name: str = "Tom"
+
+age: int = 20
+height: float = 1.75
+z: complex = 1 + 2j
+
+male: bool = True
+
+scores: list = [90, 80, 85]
+info: dict = {"name": "Tom", "age": 20}
